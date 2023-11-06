617862b575cab8eefc0c315b82c99df05f399956
Mylicoco <97513216+Mylicoco@users.noreply.github.com>
2023-11-06 11:17:15 +0000

    Add files on repository
    Adding files on a new repository

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    color: { 'bluegray': '#F1F4F8' },
+  },
+  plugins: [],
+};
